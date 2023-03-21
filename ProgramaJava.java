8285d9012895be59be7d7f12f5fe32c3c1854892
Marianna Lopes Américo <105051864+MariLopes1223@users.noreply.github.com>
2023-03-21 16:56:20 -0300

    Códigos .java e .class mencionados na atividade

---
+++ ProgramaJava.java
@@ -0,0 +1,5 @@
+class ProgramaJava {
+    public static void main(String[] args) {
+        System.out.println("Terminei a primeira aula com um programa Java!");
+    }
+}
---
+++ SegProgramaJava.java
@@ -0,0 +1,6 @@
+class SegProgramaJava {
+    public static void main(String[] args) {
+        System.out.println("Marianna Lopes Americo");
+        System.out.println("Nao torco.");
+    }
+}
